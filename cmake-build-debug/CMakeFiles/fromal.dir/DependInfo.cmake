76bf5b5031352f031a7bcebc85ee46b6b6f3da70
Arteeem4ik <artem.akhmedov2003@gmail.com>
2022-12-12 21:17:38 +0300

    Added codecov

---
+++ cmake-build-debug/CMakeFiles/fromal.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/artem/formal_prac/LongestSubstr.cpp" "/Users/artem/formal_prac/cmake-build-debug/CMakeFiles/fromal.dir/LongestSubstr.cpp.o"
+  "/Users/artem/formal_prac/main.cpp" "/Users/artem/formal_prac/cmake-build-debug/CMakeFiles/fromal.dir/main.cpp.o"
+  "/Users/artem/formal_prac/tests.cpp" "/Users/artem/formal_prac/cmake-build-debug/CMakeFiles/fromal.dir/tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/artem/googletest/googletest/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/fromal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fromal.dir/LongestSubstr.cpp.o"
+  "CMakeFiles/fromal.dir/main.cpp.o"
+  "CMakeFiles/fromal.dir/tests.cpp.o"
+  "fromal"
+  "fromal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fromal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
